8ee306f913f7b517172949ed2559878b21597c24
MikeR <mikeraadsheer11@gmail.com>
2020-08-21 16:23:52 +0200

    ????won

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ gpio.setup(7, gpio.DIR_OUT);
 
                                                                                                                                                                                         const socketToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbiI6IjJBQjAzRjI5NUY5MERDNDE5MDM5IiwicmVhZF9vbmx5Ijp0cnVlLCJwcmV2ZW50X21hc3RlciI6dHJ1ZSwidHdpdGNoX2lkIjoiNjAxNzE0MDkifQ.tFvThd__zt0YdOnI8jaUMt1M4-h9Ga4b1GNw8or5nPs";
 
-var initialized = false;
 
 const io = require('socket.io-client');
 const sl = io(`https://sockets.streamlabs.com?token=${socketToken}`, {transport: ['websocket']});
@@ -18,22 +17,13 @@ sl.on('event', (e)=>{
 });
 
 function run(){
-
-if(!initialized){
     gpio.write(7, false, (err)=>{
-        if (err) throw err;
-        console.log('7: off');
-        });
-        initialized = true;
-}
-
-    gpio.write(7, true, (err)=>{
         if (err) throw err;
         console.log('7: on');
     });
 
     setTimeout(()=> {
-        gpio.write(7, false, (err)=>{
+        gpio.write(7, true, (err)=>{
         if (err) throw err;
         console.log('7: off');
         });
