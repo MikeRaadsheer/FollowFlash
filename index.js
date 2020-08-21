6337206b0a9dab6374c00bf9e7298a8ef31ef6f0
MikeR <mikeraadsheer11@gmail.com>
2020-08-21 15:56:44 +0200

    omg so close

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var gpio = require('rpi-gpio');
 gpio.setup(7, gpio.DIR_OUT);
+TurnOff();
 
                                                                                                                                                                                         const socketToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbiI6IjJBQjAzRjI5NUY5MERDNDE5MDM5IiwicmVhZF9vbmx5Ijp0cnVlLCJwcmV2ZW50X21hc3RlciI6dHJ1ZSwidHdpdGNoX2lkIjoiNjAxNzE0MDkifQ.tFvThd__zt0YdOnI8jaUMt1M4-h9Ga4b1GNw8or5nPs";
 
@@ -19,23 +20,25 @@ sl.on('event', (e)=>{
 
 function run(){
 
-    if(a){
-        gpio.write(7, true, (err)=>{
-            if (err) throw err;
-            console.log('7: on');
-        });
-        a = false;
-    }else{
-        gpio.write(7, false, (err)=>{
-            if (err) throw err;
-            console.log('7: off');
-        });
-        a = true;
-    }
-
+    TurnOn();
     
-    // wait(5000);
+    wait(5000);
 
+    TurnOff();
+}
+
+function TurnOn(){
+    gpio.write(7, true, (err)=>{
+        if (err) throw err;
+        console.log('7: on');
+    });
+}
+
+function TurnOff(){
+    gpio.write(7, false, (err)=>{
+        if (err) throw err;
+        console.log('7: off');
+    });
 }
 
 function wait(ms){
